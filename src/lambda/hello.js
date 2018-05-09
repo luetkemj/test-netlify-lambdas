bf92a3d0c3c64f8a6ca8590ac09fba97a9a6ae75
Mark Luetke <luetkemj@gmail.com>
2018-05-08 20:24:58 -0500

    forgot that comma

--- src/lambda/hello.js
+++ src/lambda/hello.js
@@ -1,7 +1,7 @@
 exports.handler = function(event, context, callback) {
     callback(null, {
     statusCode: 200,
-    body: "Hello, World"
+    body: "Hello, World",
     envVars: process.env,
     });
 }
