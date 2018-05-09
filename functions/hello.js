8e51237b307d5d199d2cd4436337df3f2e1c6ae0
Mark Luetke <luetkemj@gmail.com>
2018-05-09 08:45:43 -0500

    reorg

---
+++ functions/hello.js
@@ -0,0 +1,9 @@
+exports.handler = function(event, context, callback) {
+    callback(null, {
+      statusCode: 200,
+      body: JSON.stringify({
+        envVars: process.env.BUTTER,
+        hello: 'world',
+      }),
+    });
+}
