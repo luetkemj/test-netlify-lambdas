3dd63b6a1e22cd3a231be10eb03e87bef7734897
Mark Luetke <luetkemj@gmail.com>
2018-05-11 16:48:12 -0500

    what if I put the require inside my handler....

--- src/functions/jwt.js
+++ src/functions/jwt.js
@@ -1,7 +1,6 @@
-// sign with default (HMAC SHA256)
-const jwt = require('jsonwebtoken');
-
 exports.handler = function(event, context, callback) {
+  const jwt = require('jsonwebtoken');
+
   const token = jwt.sign({ foo: 'barder is the best oh boy YEEHA' }, 'SECRET');
 
   callback(null, {
