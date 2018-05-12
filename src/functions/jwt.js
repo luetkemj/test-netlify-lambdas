e740b1be49b65318f0bdf5db67826d8152deafb3
Mark Luetke <luetkemj@gmail.com>
2018-05-12 15:21:44 -0500

    basic jwt auth
    extremely basic...

--- src/functions/jwt.js
+++ src/functions/jwt.js
@@ -1,21 +1,52 @@
+const jwt = require('jsonwebtoken');
+
+function meta(event, context) {
+  if (event.queryStringParameters.meta === 'true') {
+    return {
+      event,
+      context,
+    }
+  }
+}
+
+function isAuthorized(username, password) {
+  if (username === 'user' && password === 'password') {
+    return true;
+  }
+}
+
 exports.handler = function(event, context, callback) {
-  const jwt = require('jsonwebtoken');
+  const { httpMethod } = event;
 
-  const token = jwt.sign({ foo: 'barder is the best oh boy YEEHA' }, 'SECRET');
+  switch (httpMethod) {
+    case 'POST': {
+      const { username, password } = JSON.parse(event.body);
 
-  callback(null, {
-    statusCode: 200,
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      data: {
-        token,
-      },
-      meta: {
-        event,
-        context,
+      if (isAuthorized(username, password)) {
+        return callback(null, {
+          statusCode: 200,
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            data: {
+              token: jwt.sign({ username }, 'SECRET'),
+            },
+            meta: meta(event, context),
+          })
+        });
+      } else {
+        return callback(null, {
+          statusCode: 401,
+          body: 'Unauthorized',
+        });
       }
-    })
-  });
+    }
+
+    default:
+      return callback(null, {
+        statusCode: 404,
+        body: 'Not Found',
+      });
+  }
 }
